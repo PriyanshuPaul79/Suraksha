9c1692fa2a9c4bf90eb520e05746d2e7b1d61ec1
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-03-07 15:32:29 +0530

    final bug testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import {Providers} from './providers';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,7 +28,8 @@ export default function RootLayout({
           {/* navbar */}
 
         <Navbar/>
-          <main className='pt-16 '>{children}</main>
+          <main className='pt-16 '>
+            <Providers>{children}</Providers></main>
         </div>
       </body>
     </html>
