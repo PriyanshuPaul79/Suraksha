a4d637d46b731a6b5ece58b6fcdbef9eac73bbd2
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-03-14 13:07:03 +0530

    removed {}

--- app/api/reports/[reportId]/route.ts
+++ app/api/reports/[reportId]/route.ts
@@ -79,7 +79,7 @@
 
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
-import { ReportStatus } from "@prisma/client";
+import  ReportStatus  from "@prisma/client";
 import { NextRequest } from "next/server";
 
 export async function PATCH(request: NextRequest) {
